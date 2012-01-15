ab30916987ca9e544e2fc1d519b29ff248c54afc
Bryce Penberthy <bryce@servolabs.com>
2012-01-15 09:53:02 -0600

    Adding an example of the Statistics API to the CountryJpaDao test

--- src/test/java/com/servolabs/world/dao/CountryJpaDaoTest.java
+++ src/test/java/com/servolabs/world/dao/CountryJpaDaoTest.java
@@ -1,13 +1,18 @@
 package com.servolabs.world.dao;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
 import java.util.List;
 import java.util.Set;
 import java.util.logging.Logger;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.hibernate.EntityMode;
+import org.hibernate.Session;
+import org.hibernate.stat.Statistics;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,10 +28,12 @@ import com.servolabs.world.domain.Country;
 @Transactional
 public class CountryJpaDaoTest {
 	
-	private static final Logger logger = Logger.getLogger(CountryJpaDaoTest.class.getName()); 
+	private static final Logger log = Logger.getLogger(CountryJpaDaoTest.class.getName()); 
 	
 	@Autowired
 	private CountryJpaDao countryDao;
+	@PersistenceContext 
+	private EntityManager entityManager;
 	
 	// This test will demonstrate how you can change your query to force eager loading
 	// of data to occur.
@@ -34,7 +41,7 @@ public class CountryJpaDaoTest {
 	public void findAllCitiesInUS()  {
 		Country country = (Country) countryDao.find("United States");
 		Set<City> cityList = country.getCities();
-		logger.info("First City found: " + cityList.iterator().next().getName());
+		log.info("First City found: " + cityList.iterator().next().getName());
 		for(City city : cityList)  {
 			assertNotNull(city.getName());
 		}
@@ -46,6 +53,22 @@ public class CountryJpaDaoTest {
 		for(Country country : countryList)  {
 			assertEquals("North America", country.getContinent().getName().trim());
 		}
+		
+		Statistics stats = getStatistics();
+
+		double cacheHitCount = stats.getSecondLevelCacheHitCount();
+		double cacheMissCount = stats.getSecondLevelCacheMissCount();
+		double cacheHitRatio =
+		  cacheHitCount / (cacheHitCount + cacheMissCount);
+
+		log.info("Cache hit count: " + cacheHitCount);
+		log.info("Cache miss count: " + cacheMissCount);
+		log.info("Query Hit ratio:" + cacheHitRatio);
+
 	}
+
+	private Statistics getStatistics() {
+		return ((Session)entityManager.getDelegate()).getSession(EntityMode.POJO).getSessionFactory().getStatistics();
+    }
 
 }
